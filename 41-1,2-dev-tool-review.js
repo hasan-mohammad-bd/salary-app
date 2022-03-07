45c49ea814b92cdc342acf3cd160e76574eb303b
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-07 09:24:54 +0600

    adding some info

---
+++ 41-1,2-dev-tool-review.js
@@ -0,0 +1,29 @@
+/* 
+element tab review.
+device support and its other tab
+=================================
+find documentation from google. (chrome dev tools api)
+learn debug(function)
+=================================
+source tab:
+1. ctrl + p => to open files.(both source tab and vs code)
+
+2. right click on the file, you can select and deselect the 
+file according to your need.(both source tab and vs code)
+
+3. ctrl + shift + t => to take closed tab.(both source tab and vs code)it brings tab one by one.
+
+4. selecting multiple tab and pressing shift. close all the tab by pressing ctrl + w .
+
+5. click and hold the reload button, while keep opening the inspect tab. gives 3 options, which are reload, heard reload. and empty catch and heard reload.
+
+6. ctrl + shift + o, is to open function.
+
+useful tab is used for debugging.
+=>
+1. Elements
+2. Console
+3. Sources
+4. Network
+5. Application
+*/
